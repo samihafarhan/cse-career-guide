0d98efec2271914f529575e7248ac1249aa07349
Abrar Fahim <abrarfahim.inc@gmail.com>
2025-08-07 18:10:24 +0600

    demo project ideas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/landing.jsx'
 import Auth from './pages/auth.jsx'
 import MyProfile from './pages/myprofile.jsx'
+import ProjectIdeas from './pages/projectideas.jsx'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/profile" element={<MyProfile />} />
+          <Route path="/projectideas" element={<ProjectIdeas />} />
         </Routes>
       </Router>
     </div>
